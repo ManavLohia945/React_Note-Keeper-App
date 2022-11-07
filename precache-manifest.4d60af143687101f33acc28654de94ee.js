54f84e71d999ece1f3b57047c09cffdd65c67189
Manav Lohia <manavlohia945@gmail.com>
2022-11-07 10:58:12 +0530

    Updates

---
+++ precache-manifest.4d60af143687101f33acc28654de94ee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef74c6cb295cd6bea112f6b265465b2",
+    "url": "/React_Note-Keeper-App/index.html"
+  },
+  {
+    "revision": "d8bbe94d9aab3969a360",
+    "url": "/React_Note-Keeper-App/static/js/2.6ee16f53.chunk.js"
+  },
+  {
+    "revision": "618d0d5793d4a2d8a1c7",
+    "url": "/React_Note-Keeper-App/static/js/main.34e90ebe.chunk.js"
+  },
+  {
+    "revision": "46ac8e74e8f23f233a78",
+    "url": "/React_Note-Keeper-App/static/js/runtime-main.06ac423b.js"
+  }
+]);
